64813dbbcd74a8b7cecb91855b5b6d4c6d0c2b5e
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2023-05-20 20:23:21 +0530

    updated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import time
+import requests
+import json
 css_file = open("style.css", "r")
 css_text = css_file.read()
 st.markdown(f'<style>{css_text}</style>', unsafe_allow_html=True)
@@ -13,6 +15,21 @@ def analyze_sentiment():
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1)
    st.success('Analysis Finished', icon="✅")
+   url = "https://api.apilayer.com/sentiment/analysis"
+   payload = "{body}".encode("utf-8")
+   headers= {
+   "apikey": "b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"
+   }
+   response = requests.request("POST", url, headers=headers, data = payload)
+   result = response.text
+   response_data = json.loads(result)
+   sentiment = response_data["sentiment"]
+   if(sentiment == "neutral"):
+      st.warning("Your content has a neutral sentiment")
+   elif(sentiment == "postive"):
+      st.success("Your content has a postive sentiment")
+   else:
+      st.error("Your content has a negative sentiment")
 
 
 st.title("Sentimind")
@@ -23,7 +40,10 @@ with col1:
  st.write("")
 with col2:
     if (st.button("Analyse the sentiment of a text")):
-       analyze_sentiment()    
+       if txt == "":
+         st.error("The input cannot be empty")
+       else:
+          analyze_sentiment()    
 with col2:
    st.write("")
 
